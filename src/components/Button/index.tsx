abb1e85c3b8d0d59aa6955008e6ebc4f67b20003
lallaheeee <ooop0422@gmail.com>
2020-03-20 15:03:57 +0900

    [chore] 버튼 컴포넌트 스토리 작성
    story docs에 props 안떠서 삽질하다가
    props: ButtonProps 추가하니까 됐다.
    
    타입스크립트 부닥치면서 하려니까 재미없다.
    주말에 공부해야지...
    
    읽어볼 것
    https://velog.io/@velopert/create-typescript-react-component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,33 @@
-import React, { FunctionComponent, ButtonHTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes } from "react";
 import "./style.scss";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    /**
+     * 버튼에 표시될 내용
+     */
+    name: string;
+    /**
+     * 이벤트 핸들러
+     */
+    onClick: () => void;
+    /**
+     * 버튼 스타일 ex) grey, white
+     */
+    className?: string;
+}
 
-const Button: FunctionComponent<ButtonProps> = props => {
-    const { name, onClick, className = "white" } = props;
+export const Button = (props: ButtonProps) => {
+    const { name, onClick, className = "grey" } = props;
 
     return (
         <button onClick={onClick} className={className}>
             {name}
         </button>
     );
+};
+
+Button.defaultProps = {
+    className: "grey"
 };
 
 export default Button;
