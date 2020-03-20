7e0478831818d4ef36ab5e603b15eba2c04b5e3b
lallaheeee <ooop0422@gmail.com>
2020-03-20 15:06:43 +0900

    [chore] 드롭다운 스토리 작성
    스토리를 작성하면서 드롭다운 노출 여부는 부모에서
    뱄다 넣었다하면 dropdown이 아니니까
    props로 받는 게 좋겠다는 생각이 들어서visibility 속성을
    추가했다.
    
    나중에 심심하면 애니메이션 추가해야지.

---
+++ src/components/Dropdown/index.stories.tsx
@@ -0,0 +1,33 @@
+import React, { CSSProperties } from "react";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import Dropdown, { DropdownItem } from ".";
+
+export default {
+    title: "DropDown",
+    component: Dropdown,
+    decorators: [withKnobs]
+};
+
+const WrapperStyle: CSSProperties = {
+    position: "relative",
+    width: "9rem"
+};
+
+export const index = () => {
+    const items: DropdownItem[] = [
+        {
+            name: text("item1-name", "마이페이지"),
+            onClick: action("마이페이지로 이동")
+        },
+        {
+            name: text("item2-name", "로그아웃"),
+            onClick: action("로그아웃")
+        }
+    ];
+    return (
+        <div style={WrapperStyle}>
+            <Dropdown items={items} visibility={boolean("visibility", true)} />
+        </div>
+    );
+};
