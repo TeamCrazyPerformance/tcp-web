fb16461570d5eaa3b066538d1a1141c4c592a3b7
lallaheeee <ooop0422@gmail.com>
2020-03-20 14:49:48 +0900

    [chore] 컴포넌트 개발시 이용할 스토리북 설정
    addons 판넬 안보여서 한시간 삽질했는데 캐싱때문이었다..
    강력 새로고침하니까 보였다..
    
    docs props에 no props 떠서 왜 안뜨지 삽질했던 건
    아직 타입스크립트가 미숙하기 때문..

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
     stories: ["../src/**/*.stories.(ts|tsx|js|jsx)"],
     addons: [
         "@storybook/preset-create-react-app",
+        "@storybook/addon-knobs/register",
         "@storybook/addon-actions",
         "@storybook/addon-links",
         {
