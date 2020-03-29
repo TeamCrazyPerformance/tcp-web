0f371cd6db34bc3fc01b1fc8fa62ac8dc4f4cac6
lallaheeee <ooop0422@gmail.com>
2020-03-29 15:48:33 +0900

    [feat] user type 정의
    i-prefix를 붙이는 것에 대하여 더 공부할 것..!
    https://github.com/microsoft/TypeScript-Handbook/issues/121
    https://stackoverflow.com/questions/31876947/confused-about-the-interface-and-class-coding-guidelines-for-typescript/41967120#41967120

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+export interface User {
+    githubId: string;
+    email: string;
+    username: string;
+    avatar: string;
+    exist: boolean;
+}
+
+export interface Profile {
+    grade: number;
+    schoolRegister: string;
+    phone: string;
+    birth: string;
+    username: string;
+    email: string;
+    blog: string;
+}
