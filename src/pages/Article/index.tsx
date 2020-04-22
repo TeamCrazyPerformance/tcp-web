7677839e7c48ef8c06b40878a5f4fa06f3bac470
lallaheeee <ooop0422@gmail.com>
2020-04-22 12:06:17 +0900

    [feat] 댓글의 삭제 버튼을 눌러 삭제 구현
    본인 아닐 시 버튼 노출 X
    에러 처리 필요

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { useArticle, ArticleProvider } from "~/contexts/Article";
+import {
+    useArticle,
+    ArticleProvider,
+    Action as ArticleAction,
+} from "~/contexts/Article";
 import { useCategory, CategoryProvider } from "~/contexts/Category";
 import { useAuth } from "~/contexts/Auth";
+import * as api from "~/apis/Comment";
 import View from "./view";
 
+//TODO : api 에러시 토스트/모달 띄우기
 const Article = () => {
     const {
         state: { article, comments },
+        dispatch: ArticleDispatch,
     } = useArticle();
 
     const {
@@ -17,6 +24,14 @@ const Article = () => {
         state: { user },
     } = useAuth();
 
+    const handleCommentDelete = (commentId: number) => {
+        if (!article) return;
+
+        api.deleteComment(article.id, commentId).then(() =>
+            ArticleDispatch({ type: ArticleAction.DELETE_COMMENT, commentId })
+        );
+    };
+
     if (!(article && user)) return null;
     return (
         <View
@@ -24,6 +39,7 @@ const Article = () => {
             comments={comments}
             categories={categories}
             user={user}
+            onCommentDelete={handleCommentDelete}
         />
     );
 };
