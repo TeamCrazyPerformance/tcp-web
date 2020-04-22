714be4367bbe18ad98a96cf6c343fc07d7aa1335
lallaheeee <ooop0422@gmail.com>
2020-04-22 11:45:29 +0900

    [fix] 댓글에 수정, 삭제 이벤트 핸들러 추가
    props의 depth이 되는 기적.. 괜찮은 거  같기두 하고..

--- src/components/molecules/CommentCard/index.tsx
+++ src/components/molecules/CommentCard/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Avatar from "@atoms/Avatar";
 import { FiEdit3 as EditIcon } from "react-icons/fi";
-import { IoMdClose as CloseIcon } from "react-icons/io";
+import { IoMdClose as DeleteIcon } from "react-icons/io";
 import { displayDate } from "~/utils";
 import { Comment } from "~/types";
 import "./style.scss";
@@ -16,16 +16,26 @@ export interface CommentCardProps {
      * 수정 버튼 노출 여부
      */
     editable?: boolean;
+    /**
+     * 댓글 삭제 이벤트
+     */
+    onDelete?: (commentId: number) => void;
+    /**
+     * 댓글 수정 이벤트
+     */
+    onEdit?: (comment: { id: number; contents: string }) => void;
 }
 
 const CommentCard = (props: CommentCardProps) => {
-    const { comment, editable, deletable } = props;
+    const { comment, editable, deletable, onDelete } = props;
     const { author, createdAt, updatedAt, contents } = comment;
     const { avatar = "", github = "", username = "" } = author;
 
     const dateView = displayDate(updatedAt || createdAt, {
         format: "YYYY. MM. DD A hh:mm",
     });
+
+    const handleDelete = () => onDelete && onDelete(comment.id);
 
     return (
         <div className="box_comment_card">
@@ -41,7 +51,7 @@ const CommentCard = (props: CommentCardProps) => {
                     </span>
                     <span className="comment_btns">
                         {editable && <EditIcon className="edit" />}
-                        {deletable && <CloseIcon />}
+                        {deletable && <DeleteIcon onClick={handleDelete} />}
                     </span>
                 </div>
                 <div className="comment_body">
--- src/pages/Article/view.tsx
+++ src/pages/Article/view.tsx
@@ -5,10 +5,26 @@ import { default as ArticleView, ArticleProps } from "@organisms/Article";
 import { Category } from "~/types";
 import "./style.scss";
 
-export type Props = ArticleProps & { categories: Category[] };
+export type Props = ArticleProps & { categories: Category[] } & {
+    /**
+     * 댓글 삭제 이벤트
+     */
+    onCommentDelete?: (id: number) => void;
+    /**
+     * 댓글 수정 이벤트
+     */
+    onCommentEdit?: (comment: { id: number; contents: string }) => void;
+};
 
 const Article = (props: Props) => {
-    const { article, comments, categories, user } = props;
+    const {
+        article,
+        comments,
+        categories,
+        user,
+        onCommentDelete,
+        onCommentEdit,
+    } = props;
 
     if (!(article && user)) return null;
 
@@ -24,6 +40,8 @@ const Article = (props: Props) => {
                         article={article}
                         comments={comments}
                         user={user}
+                        onCommentEdit={onCommentEdit}
+                        onCommentDelete={onCommentDelete}
                     />
                 </main>
             </div>
