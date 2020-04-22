af50ccded0bc36991255da0680210cf93582c247
lallaheeee <ooop0422@gmail.com>
2020-04-22 11:19:12 +0900

    [feat(api)] 댓글 api 레이어 작성
    댓글 생성, 수정, 삭제 api 작성

---
+++ src/apis/Comment.ts
@@ -0,0 +1,33 @@
+import API from "./utils";
+import { Comment as IComment } from "~/types";
+
+type Comment = {
+    comment: IComment;
+};
+
+export function createComment(
+    articleId: string | number,
+    category: { category: number },
+    comment: { contents: string }
+) {
+    return API.post<Comment>(`/articles/${articleId}/comments`, {
+        comment,
+        category,
+    }).then(res => res.data);
+}
+
+export function updateComment(
+    articleId: string | number,
+    comment: {
+        id: number;
+        contents: string;
+    }
+) {
+    return API.patch<Comment>(`/articles/${articleId}/comments/${comment.id}`, {
+        comment,
+    }).then(res => res.data);
+}
+
+export function deleteComment(articleId: string | number, commentId: number) {
+    return API.delete<null>(`/articles/${articleId}/comments/${commentId}`);
+}
