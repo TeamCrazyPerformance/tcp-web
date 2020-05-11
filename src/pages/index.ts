d7f5c4fe2d6fd964ba4e80e2fd128a6c4b5cb35b
lallaheeee <ooop0422@gmail.com>
2020-05-11 18:41:09 +0900

    [chore(router)] 에디터 라우터 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,6 +2,7 @@ export { default as Home } from "./Home";
 export { default as SignUp } from "./SignUp";
 export { default as ArticleList } from "./ArticleList";
 export { default as Article } from "./Article";
+export { default as Editor } from "./Editor";
 export { default as MemberList } from "./MemberList";
 export { default as MyPage } from "./MyPage";
 export { default as NotFound } from "./NotFound";
