101ed683deb3b64d75d84d8b59903528664fac3d
lallaheeee <ooop0422@gmail.com>
2020-05-11 18:38:29 +0900

    [fix] 에디터 컴포넌트에 카테고리 추가
    카테고리 노출하도록 추가
    및 리팩토링

--- src/components/organisms/Editor/index.tsx
+++ src/components/organisms/Editor/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useMemo } from "react";
 import Button from "@atoms/Button";
 import Select from "@molecules/FormSelect";
 import Input from "@molecules/FormInput";
 import ReactEditor from "@molecules/Editor";
+import { Category } from "~/types";
+
+const TITLE = "title";
+const CONTENTS = "contents";
 
 const selectProps = {
     labelName: "카테고리",
     invalid: false,
     invalidCaption: "카테고리를 선택해주세요",
-    options: [],
 };
 
 const inputProps = {
@@ -18,17 +21,52 @@ const inputProps = {
 };
 
 export type FORM_KEYS = "title" | "contents";
-
+type Accumulator = { value: number; contents: string }[];
 export interface EditorProps {
-    categories?: [];
+    categories?: Category[];
+    initialValue?: { title: string; contents: string };
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
     handleChange: (payload: { key: FORM_KEYS; value: string }) => void;
-    initialValue?: string;
+    handleCategoryChange: (seletedTab: number) => void;
 }
+
+const convertCategoryToOptions = (categories: Category[]): Accumulator => {
+    return categories.reduce((acc: Accumulator, cur: Category) => {
+        const { id: value, name: contents, subItems } = cur;
+        if (subItems) {
+            return [
+                ...acc,
+                { value, contents },
+                ...subItems.map(({ id: value, name }) => ({
+                    value,
+                    contents: `${contents}/${name}`,
+                })),
+            ];
+        }
+
+        return [...acc, { value, contents }];
+    }, []);
+};
 
 //TODO category 리스트 추가, 제목 추가
 const Editor = (props: EditorProps) => {
-    const { handleSubmit, initialValue = "", handleChange } = props;
+    const {
+        handleSubmit,
+        initialValue = { title: "", contents: "" },
+        handleChange,
+        categories = [],
+        handleCategoryChange,
+    } = props;
+
+    const options = useMemo(() => convertCategoryToOptions(categories), [
+        categories,
+    ]);
+
+    const handleChangeCategory = (e: React.FocusEvent<HTMLSelectElement>) => {
+        const { value } = e.target;
+
+        handleCategoryChange(parseInt(value));
+    };
 
     const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -36,20 +74,28 @@ const Editor = (props: EditorProps) => {
     };
 
     const handleChangeContents = (e: React.ChangeEvent<HTMLInputElement>) => {
-        handleChange({ key: "title", value: e.currentTarget.value });
+        handleChange({ key: TITLE, value: e.target.value });
     };
 
     const handleChangeTitle = (value: string) => {
-        handleChange({ key: "contents", value });
+        handleChange({ key: CONTENTS, value });
     };
 
     return (
         <form onSubmit={handleOnSubmit}>
-            <Select {...selectProps} />
-            <Input {...inputProps} onChange={handleChangeContents} />
+            <Select
+                {...selectProps}
+                options={options}
+                onBlur={handleChangeCategory}
+            />
+            <Input
+                {...inputProps}
+                defaultValue={initialValue.title}
+                onChange={handleChangeContents}
+            />
             <ReactEditor
                 handleChange={handleChangeTitle}
-                initialValue={initialValue}
+                initialValue={initialValue.contents}
             />
             <Button name="등록" />
         </form>
