100f6f5531f4d6a584a4e25efe73cb6c24806c98
lallaheeee <ooop0422@gmail.com>
2020-05-22 21:40:39 +0900

    [feat(ui)] #37 404 not found 작성

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "@organisms/Header";
+import "./style.scss";
 
 const NotFound = () => (
     <>
         <Header />
-        <main>welcome, NotFound page!</main>
+        <main className="not_found">
+            <p>
+                We&apos;re Sorry
+                <span role="img" aria-label="Chick">
+                    🐣
+                </span>
+            </p>
+            <p>We can&apos;t Find it. please try again.</p>
+        </main>
     </>
 );
 
