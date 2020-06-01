b7c23e31672c2ac2f5f88909e3854817ad969200
lallaheeee <ooop0422@gmail.com>
2020-06-01 15:39:18 +0900

    [feat] base template 작성
    템플릿에 공통으로 들어가는 baseTemplate 작성

---
+++ src/components/template/Base/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import Header from "~/components/UI/blocks/Header";
+import SideNavigation from "~/components/UI/atoms/SideNavigation";
+import { Category } from "~/types";
+// import "./style.scss";
+
+interface Props {
+    categories: Category[];
+    children: ReactNode
+}
+
+const BaseTemplate = (props: Props) => {
+    const {
+        categories,
+        children,
+    } = props;
+
+    return (
+        <>
+            <Header />
+            <div className="body_wrapper">
+                <nav>
+                    <SideNavigation items={categories} />
+                </nav>
+                <main>
+                    {children}
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default BaseTemplate;
