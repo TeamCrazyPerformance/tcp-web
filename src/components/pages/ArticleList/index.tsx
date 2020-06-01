3fc55da37ec6a8a48a959c9f8779d094084754a9
lallaheeee <ooop0422@gmail.com>
2020-06-01 15:55:35 +0900

    [refactor] 게시글 목록 페이지 템플릿으로 변경
    기존의 bulletin board로 한 뎁쓰 나눈게 관리가 어려워
    template으로 변경

--- src/components/pages/ArticleList/index.tsx
+++ src/components/pages/ArticleList/index.tsx
@@ -1,16 +1,47 @@
 import React from "react";
-import { ArticleListProvider } from "~/contexts/ArticleList";
-import { CategoryProvider } from "~/contexts/Category";
-import View from "./view";
+import ArticleListView from "~/components/template/ArticleList";
+import { CategoryProvider, useCategory } from "~/contexts/Category";
+import { useAuth } from "~/contexts/Auth";
+import {
+    ArticleListProvider,
+    useArticleList,
+    Action as ArticleListAction,
+} from "~/contexts/ArticleList";
 
-const ArticleList = () => {
+const View = () => {
+    const {
+        state: { notices, articles, articlesCount },
+        dispatch: articleListDispatch,
+    } = useArticleList();
+    const {
+        state: { categories },
+    } = useCategory();
+
+    const {
+        state: { user },
+    } = useAuth();
+
     return (
-        <CategoryProvider>
-            <ArticleListProvider>
-                <View />
-            </ArticleListProvider>
-        </CategoryProvider>
+        <ArticleListView
+            categories={categories}
+            user={user}
+            articles={articles}
+            notices={notices}
+            articlesCount={articlesCount}
+            onPageChange={({ selected: page }) => {
+                articleListDispatch({
+                    type: ArticleListAction.SET_PAGE,
+                    page,
+                });
+            }}
+        />
     );
 };
 
-export default ArticleList;
+export default () => (
+    <CategoryProvider>
+        <ArticleListProvider>
+            <View />
+        </ArticleListProvider>
+    </CategoryProvider>
+);
